4371f44089634ff1598fba550eb757967cdfa44d
hwyn <237617561@qq.com>
2023-03-23 20:21:03 +0800

    update

--- providers/platform/index.d.ts
+++ providers/platform/index.d.ts
@@ -2,8 +2,8 @@ import { Provider } from '@fm/di';
 import { ExpressServerPlatform } from './platform';
 export { PLATFORM_SCOPE } from '@fm/core/providers/platform';
 export declare const dyanmicServer: (port: number, providers?: Provider[]) => ExpressServerPlatform;
-export declare const Application: (this: unknown, ...args: any[]) => (cls: import("@fm/di").Type<any>) => any;
-export declare const Prov: (token: import("@fm/di").TokenKey, provider?: {
+export declare const Application: (this: unknown, ...args: any[]) => (cls: import("../../../di").Type<any>) => any;
+export declare const Prov: (token: import("../../../di").TokenKey, provider?: {
     [key: string]: any;
     providedIn?: string;
 }) => any;
