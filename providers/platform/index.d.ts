1b4bd702ff857a1f44d64dd59eecf8edcb67dd2a
hwyn <237617561@qq.com>
2023-04-10 22:24:13 +0800

    update

--- providers/platform/index.d.ts
+++ providers/platform/index.d.ts
@@ -2,8 +2,8 @@ import { Provider } from '@fm/di';
 import { ExpressServerPlatform } from './platform';
 export { PLATFORM_SCOPE } from '@fm/core/providers/platform';
 export declare const dynamicServer: (port: number, providers?: Provider[]) => ExpressServerPlatform;
-export declare const Application: <M extends import("../..").MetadataInfo>(metadata: {
-    [key: string]: Record<string, any>;
+export declare const Application: <M extends import("../..").MetadataInfo>(metadata?: {
+    [key: string]: any;
 } | import("../../../di").Type<M>) => <T = any>(cls: import("../../../di").Type<T>) => import("../../../di").Type<T>;
 export declare const Prov: (token: import("../../../di").TokenKey, provider?: {
     [key: string]: any;
